54437e8d1f42429db92ddeb44ea339abc25106e1
Katelyn Junker <kjunker@villanova.edu>
2018-11-01 22:51:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentloaded", (e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
   const titanicButton = document.querySelector('#Titanic')
-  const terminatorButton = document.querySelector('#Terminator 2')
+  const terminatorButton = document.querySelector('#Terminator-2')
   
   titanicButton.addEventListener('#click',)
   
